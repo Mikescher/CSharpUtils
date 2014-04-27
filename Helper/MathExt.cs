aa0b7d30b8fe1e6ebd43d59fc5a06848c6ac3c5c
Mike Schwörer <mailport@mikescher.de>
2014-04-27 20:14:55 +0200

    Added various classes from other projects

---
+++ Helper/MathExt.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace MSHC.Helper
+{
+	public class MathExt
+	{
+		public static void Swap<T>(ref T lhs, ref T rhs)
+		{
+			T temp;
+			temp = lhs;
+			lhs = rhs;
+			rhs = temp;
+		}
+
+		public static int Max(int v1, params int[] va)
+		{
+			foreach (int v in va)
+			{
+				v1 = Math.Max(v1, v);
+			}
+			return v1;
+		}
+
+		public static int Min(int v1, params int[] va)
+		{
+			foreach (int v in va)
+			{
+				v1 = Math.Min(v1, v);
+			}
+			return v1;
+		}
+
+		public static long IntPow(int x, short power)
+		{
+			if (power == 0)
+				return 1;
+			if (power == 1)
+				return x;
+			// ----------------------
+			int n = 15;
+			while ((power <<= 1) >= 0)
+				n--;
+
+			long tmp = x;
+			while (--n > 0)
+				tmp = tmp * tmp *
+					 (((power <<= 1) < 0) ? x : 1);
+			return tmp;
+		}
+
+	}
+}
