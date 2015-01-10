38494fe6b5d16a4a191b76365fcfae554ca4fc10
Mike Schwörer <mailport@mikescher.de>
2015-01-11 00:53:54 +0100

    stuff

---
+++ Extensions/ListExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace MSHC.Extensions
+{
+	public static class ListExtensions
+	{
+		public static IEnumerable<T> SkipLastN<T>(this IEnumerable<T> source, int n)
+		{
+			var it = source.GetEnumerator();
+			bool hasRemainingItems = false;
+			var cache = new Queue<T>(n + 1);
+
+			do
+			{
+				if (hasRemainingItems = it.MoveNext())
+				{
+					cache.Enqueue(it.Current);
+					if (cache.Count > n)
+						yield return cache.Dequeue();
+				}
+			} while (hasRemainingItems);
+		}
+	}
+}
