d2471ac798d15ba9acacfa048c3b7c048e778058
Mike Schwörer <mailport@mikescher.de>
2017-02-08 18:04:11 +0100

    Various classes:  - XML Helper  - EnumSource MarkupExtension  - AES Encryption  - Multithreading stuff  - WPF DialogWindow (Resizable but no Min/Max Buttons)  - Binable Passwordbox

---
+++ Helper/XHelper.cs
@@ -0,0 +1,93 @@
+﻿using MSHC.Extensions;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace MSHC.Helper
+{
+	public static class XHelper
+	{
+		#region GetChildValue
+
+		public static string GetChildValue(XElement parent, string childName, string defaultValue)
+		{
+			var child = parent.Elements(childName).FirstOrDefault();
+			if (child == null) return defaultValue;
+
+			return child.Value;
+		}
+
+		public static int GetChildValue(XElement parent, string childName, int defaultValue)
+		{
+			var child = parent.Elements(childName).FirstOrDefault();
+			if (child == null) return defaultValue;
+
+			return int.Parse(child.Value);
+		}
+
+		public static int? GetChildValue(XElement parent, string childName, int? defaultValue)
+		{
+			var child = parent.Elements(childName).FirstOrDefault();
+			if (child == null) return defaultValue;
+
+			if (child.Value.Trim() == "") return null;
+
+			return int.Parse(child.Value);
+		}
+
+		public static bool GetChildValue(XElement parent, string childName, bool defaultValue)
+		{
+			var child = parent.Elements(childName).FirstOrDefault();
+			if (child == null) return defaultValue;
+
+			return XElementExtensions.ParseBool(child.Value);
+		}
+
+		public static Guid GetChildValue(XElement parent, string childName, Guid defaultValue)
+		{
+			var child = parent.Elements(childName).FirstOrDefault();
+			if (child == null) return defaultValue;
+
+			return Guid.Parse(child.Value);
+		}
+
+		public static TEnumType GetChildValue<TEnumType>(XElement parent, string childName, TEnumType defaultValue) where TEnumType : struct, IComparable, IFormattable, IConvertible
+		{
+			var child = parent.Elements(childName).FirstOrDefault();
+			if (child == null) return defaultValue;
+
+			int value;
+			TEnumType evalue;
+			if (int.TryParse(child.Value, out value))
+			{
+				foreach (var enumValue in Enum.GetValues(typeof(TEnumType)))
+				{
+					if (value == Convert.ToInt32(Enum.Parse(typeof(TEnumType), enumValue.ToString())))
+						return (TEnumType)enumValue;
+				}
+			}
+			if (Enum.TryParse(child.Value, true, out evalue))
+			{
+				return evalue;
+			}
+
+			throw new ArgumentException("'"+child.Value+"' is not a valid value for Enum");
+		}
+
+		#endregion
+		
+		public static string ConvertToString(XDocument doc)
+		{
+			if (doc == null) throw new ArgumentNullException("doc");
+
+			StringBuilder builder = new StringBuilder();
+			using (TextWriter writer = new StringWriter(builder))
+			{
+				doc.Save(writer);
+			}
+			return builder.ToString();
+		}
+	}
+}
---
+++ Threads/SingleThreadPool.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace MSHC.Threads
+{
+	public class SingleThreadPool
+	{
+		private readonly object syncLock = new object();
+
+		private readonly Action<int> setCount;
+		private readonly Action<Exception> errorCallback;
+
+		private readonly ConcurrentStack<Action> queue = new ConcurrentStack<Action>(); 
+		private Thread executor;
+
+		public SingleThreadPool(Action<int> onChange, Action<Exception> onError)
+		{
+			executor = new Thread(Run);
+
+			setCount = onChange;
+			errorCallback = onError;
+		}
+
+		private void Start()
+		{
+			if (executor == null || !executor.IsAlive)
+			{
+				executor = new Thread(Run);
+				executor.IsBackground = true;
+				executor.Start();
+			}
+		}
+
+		public void QueueAction(Action a)
+		{
+			lock (syncLock)
+			{
+				queue.Push(a);
+				if (setCount != null) setCount(queue.Count);
+				if (executor == null || !executor.IsAlive) Start();
+			}
+		}
+
+		private void Run()
+		{
+			for (;;)
+			{
+				Action action;
+
+				lock (syncLock)
+				{
+					bool exec = queue.TryPop(out action);
+					if (!exec)
+					{
+						if (setCount != null) setCount(queue.Count);
+						return;
+					}
+				}
+
+				try
+				{
+					action.Invoke();
+					if (setCount != null) setCount(queue.Count);
+				}
+				catch (Exception e)
+				{
+					if (errorCallback != null) errorCallback(e);
+					return;
+				}
+			}
+		}
+	}
+}
