164aceb0b818ffc840dbd8a05ab9be79f82c9191
Mike Schwörer <mailport@mikescher.de>
2017-02-15 21:38:12 +0100

    PKBDF2 class from medo64.com

---
+++ Util/Helper/FilenameHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace MSHC.Util.Helper
+{
+	public static class FilenameHelper
+	{
+		private const char CONVERT_ESCAPE_CHARACTER = '%';
+		private const string ALLOWED_CHARACTER = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~#+-_.,;%& {}()=ÄÖÜäöüµ@";
+
+		public static string ConvertStringForFilename(string input)
+		{
+			StringBuilder output = new StringBuilder(input.Length);
+
+			foreach (var c in input)
+			{
+				if (c == CONVERT_ESCAPE_CHARACTER)
+				{
+					output.Append(c);
+					output.Append(string.Format("{0:X4}", (int)c));
+				}
+				else if (ALLOWED_CHARACTER.Contains(c))
+				{
+					output.Append(c);
+				}
+				else
+				{
+					output.Append(c);
+					output.Append(string.Format("{0:X4}", (int)c));
+				}
+			}
+
+			return output.ToString();
+		}
+	}
+}
