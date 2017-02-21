27e5d1b00b4e65a38f175e1ebe85b5613c969bb4
Mike Schwörer <mailport@mikescher.de>
2017-02-21 20:42:03 +0100

    Added GZWebClient

--- Util/Helper/FilenameHelper.cs
+++ Util/Helper/FilenameHelper.cs
@@ -16,7 +16,7 @@ namespace MSHC.Util.Helper
 			{
 				if (c == CONVERT_ESCAPE_CHARACTER)
 				{
-					output.Append(c);
+					output.Append(CONVERT_ESCAPE_CHARACTER);
 					output.Append(string.Format("{0:X4}", (int)c));
 				}
 				else if (ALLOWED_CHARACTER.Contains(c))
@@ -25,9 +25,21 @@ namespace MSHC.Util.Helper
 				}
 				else
 				{
-					output.Append(c);
+					output.Append(CONVERT_ESCAPE_CHARACTER);
 					output.Append(string.Format("{0:X4}", (int)c));
 				}
+			}
+
+			return output.ToString();
+		}
+
+		public static string StripStringForFilename(string input)
+		{
+			StringBuilder output = new StringBuilder(input.Length);
+
+			foreach (var c in input)
+			{
+				if (ALLOWED_CHARACTER.Contains(c)) output.Append(c);
 			}
 
 			return output.ToString();
