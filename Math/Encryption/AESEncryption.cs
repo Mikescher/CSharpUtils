aa6f1c53189799581524d68c77fef0e8b56573a1
Mike Schwörer <mailport@mikescher.de>
2017-02-22 20:48:34 +0100

    Added AES:CBC:256

---
+++ Math/Encryption/AESEncryption.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace MSHC.Math.Encryption
+{
+	public static class AESEncryption
+	{
+		public static byte[] DecryptCBC256(byte[] data, byte[] key, byte[] iv)
+		{
+			if (iv == null) iv = new byte[16];
+
+			using (var rijndaelManaged = new RijndaelManaged { Key = key, IV = iv, Mode = CipherMode.CBC })
+			using (var memoryStream = new MemoryStream(data))
+			using (var cryptoStream = new CryptoStream(memoryStream, rijndaelManaged.CreateDecryptor(key, iv), CryptoStreamMode.Read))
+			{
+				byte[] buffer = new byte[16 * 1024];
+				using (MemoryStream ms = new MemoryStream())
+				{
+					int read;
+					while ((read = cryptoStream.Read(buffer, 0, buffer.Length)) > 0)
+					{
+						ms.Write(buffer, 0, read);
+					}
+					return ms.ToArray();
+				}
+			}
+		}
+	}
+}
