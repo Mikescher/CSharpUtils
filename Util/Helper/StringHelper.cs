9c4371c0c3e3bfa5bd1ac943dfc550e5f3963a49
Mike Schwörer <mailport@mikescher.de>
2019-03-14 01:10:59 +0100

    StringHelper.ReplaceFirst

---
+++ Util/Helper/StringHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MSHC.Util.Helper
+{
+	public static class StringHelper
+	{
+		// https://stackoverflow.com/a/8809437/1761622
+		public static string ReplaceFirst(string text, string search, string replace)
+		{
+			var pos = text.IndexOf(search, StringComparison.Ordinal);
+			if (pos < 0) return text;
+			return text.Substring(0, pos) + replace + text.Substring(pos + search.Length);
+		}
+	}
+}
