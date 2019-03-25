923b40e644c8fb52d74fcaf6cad248e33f4e70b7
Mike Schwörer <mailport@mikescher.de>
2019-03-25 16:49:58 +0100

    Added converter: IntegerEqualsToVisibility

---
+++ WPF/Converter/Visibility/IntegerEqualsToVisibility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+using MSHC.WPF.MVVM;
+
+namespace MSHC.WPF.Converter
+{
+	public class IntegerEqualsToVisibility : OneWayConverter<int, Visibility>
+	{
+		protected override Visibility Convert(int value, object parameter)
+		{
+			var v  = int.Parse(parameter.ToString().Split(';')[0]);
+			var p1 = (Visibility)Enum.Parse(typeof(Visibility), parameter.ToString().Split(';')[0]);
+			var p2 = (Visibility)Enum.Parse(typeof(Visibility), parameter.ToString().Split(';')[0]);
+
+			return (value == v) ? p1 : p2;
+
+		}
+	}
+}
