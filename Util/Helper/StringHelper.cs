a071cc256c112192f7cd49efedad99cf0f21b2e6
Mike Schwörer <mailport@mikescher.de>
2019-06-17 21:25:18 +0200

    RemoveDiacritics

--- Util/Helper/StringHelper.cs
+++ Util/Helper/StringHelper.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Text;
 
 namespace MSHC.Util.Helper
 {
@@ -10,6 +12,20 @@ namespace MSHC.Util.Helper
 			var pos = text.IndexOf(search, StringComparison.Ordinal);
 			if (pos < 0) return text;
 			return text.Substring(0, pos) + replace + text.Substring(pos + search.Length);
+		}
+
+		// https://stackoverflow.com/a/368850/1761622
+		public static string RemoveDiacritics(string text)
+		{
+			var formD = text.Normalize(NormalizationForm.FormD);
+			var sb = new StringBuilder();
+
+			foreach (char ch in formD)
+			{
+				if (CharUnicodeInfo.GetUnicodeCategory(ch) != UnicodeCategory.NonSpacingMark) sb.Append(ch);
+			}
+
+			return sb.ToString().Normalize(NormalizationForm.FormC);
 		}
 	}
 }
