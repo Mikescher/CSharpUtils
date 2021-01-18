bf79ea0e318c6f49c2d620c293599cd8c80ed18a
Mike Schwörer <pubgit@mikescher.com>
2021-01-18 12:37:51 +0100

    Added BoolToGridLength

---
+++ WPFUtils/WPF/Converter/Other/BoolToGridLength.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using MSHC.WPF.MVVM;
+
+namespace MSHC.WPF.Converter
+{
+	public class BoolToGridLength : OneWayConverter<bool, GridLength>
+	{
+		private static readonly GridLengthConverter _conv = new GridLengthConverter();
+
+		protected override GridLength Convert(bool value, object parameter)
+		{
+			if (value) return (GridLength)_conv.ConvertFromString(parameter.ToString().Split(';')[0]);
+			else       return (GridLength)_conv.ConvertFromString(parameter.ToString().Split(';')[1]);
+		}
+	}
+}
