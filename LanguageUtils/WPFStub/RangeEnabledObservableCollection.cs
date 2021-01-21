425e5f481d2664f13c8cbb13cd5785cc320da80d
Mike Schwörer <pubgit@mikescher.com>
2021-01-21 09:47:20 +0100

    Rename RangeEnabledObservableCollection.InsertRange to AddRange

--- LanguageUtils/WPFStub/RangeEnabledObservableCollection.cs
+++ LanguageUtils/WPFStub/RangeEnabledObservableCollection.cs
@@ -7,7 +7,7 @@ namespace MSHC.WPF.MVVM
 {
 	public class RangeEnabledObservableCollection<T> : ObservableCollection<T>
 	{
-		public void InsertRange(IEnumerable<T> items) 
+		public void AddRange(IEnumerable<T> items) 
 		{
 			this.CheckReentrancy();
 			foreach(var item in items) this.Items.Add(item);
