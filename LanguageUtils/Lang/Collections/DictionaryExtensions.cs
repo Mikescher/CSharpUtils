88267a5bab0d9aab85c7439b491499c5784fb781
Mike Schwörer <pubgit@mikescher.com>
2021-01-21 07:04:43 +0100

    Added DictionaryExtensions.AddIfNotContains

---
+++ LanguageUtils/Lang/Collections/DictionaryExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace MSHC.Lang.Collections
+{
+    public static class DictionaryExtensions
+    {
+        public static bool AddIfNotContains<TKey, TVal>(this IDictionary<TKey, TVal> dict, TKey key, TVal val)
+        {
+            if (dict.ContainsKey(key)) return false;
+            dict.Add(key, val);
+            return true;
+        }
+    }
+}
